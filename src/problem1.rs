cd38ff01601ebd219cda73d44ab0781c816524e0
Samuel Leeuwenburg <samleeuwenburg@gmail.com>
2019-10-23 23:02:32 +0200

    problem 3

--- src/problem1.rs
+++ src/problem1.rs
@@ -1,8 +1,10 @@
+#![allow(dead_code)]
+
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 // The sum of these multiples is 23.
 //
 // Find the sum of all the multiples of 3 or 5 below 1000.
-pub fn _problem_1() -> usize {
+pub fn problem() -> usize {
     (1..1000).fold(0, |acc, x| {
         if x % 3 == 0 || x % 5 == 0 {
             acc + x
@@ -19,7 +21,7 @@ mod tests {
 
     #[test]
     fn correct_result() {
-        assert_eq!(_problem_1(), 233168);
+        assert_eq!(problem(), 233168);
     }
 }
 
--- src/problem3.rs
+++ src/problem3.rs
@@ -1,40 +1,115 @@
+#![allow(dead_code)]
+use std::collections::HashMap;
+
 // The prime factors of 13195 are 5, 7, 13 and 29.
 //
 // What is the largest prime factor of the number 600851475143 ?
-pub fn _problem_3() {
+fn problem(num: u64) -> u64 {
+    let prime_factors = get_prime_factorization(num);
+    prime_factors.last().cloned().unwrap()
 }
 
-fn _is_prime(num: usize) -> bool {
-    let round_division = (2..num)
-        .into_iter()
-        .find(|x| (num as f32 / *x as f32).fract() == 0f32);
-       
-    match round_division {
-        Some(_) => false,
-        None => true
+fn get_prime_factorization(num: u64) -> Vec<u64> {
+    let primes = Primes::new();
+
+    fn rec(mut primes: Primes, mut factors: Vec<u64>, x: u64) -> Vec<u64> {
+        if primes.is_prime(x) {
+            factors.push(x);
+            return factors
+        }
+
+        let factor = (2..x)
+            .find_map(|y| {
+                if !primes.is_prime(y) {
+                    return None
+                }
+
+                let is_round_division = (x as f64 / y as f64).fract() == 0f64;
+
+                if !is_round_division {
+                    return None
+                }
+
+                Some((x / y, y))
+            });
+
+        match factor {
+            Some((remainder, factor)) => {
+                factors.push(factor);
+                rec(primes, factors, remainder)
+            }
+            None => {
+                factors 
+            }
+        }
+    }
+
+    rec(primes, vec![], num)
+}
+
+struct Primes {
+    found_primes: HashMap<u64, bool>,
+}
+
+impl Primes {
+    fn new() -> Primes {
+        Primes { found_primes: HashMap::new() }
+    }
+
+    fn is_prime(&mut self, num: u64) -> bool {
+        match self.found_primes.get(&num) {
+            Some(is_prime) => *is_prime,
+            None => {
+                let round_division = (2..num)
+                    .into_iter()
+                    .find(|x| (num as f64 / *x as f64).fract() == 0f64);
+
+                let is_prime = match round_division {
+                    Some(_) => false,
+                    None => true
+                };
+
+                self.found_primes.insert(num, is_prime);
+                is_prime
+            }
+        }
     }
 }
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn is_prime_works() {
-        assert_eq!(_is_prime(2), true);
-        assert_eq!(_is_prime(3), true);
-        assert_eq!(_is_prime(5), true);
-        assert_eq!(_is_prime(7), true);
-        assert_eq!(_is_prime(13), true);
-        assert_eq!(_is_prime(19), true);
-        assert_eq!(_is_prime(29), true);
-        assert_eq!(_is_prime(97), true);
+    fn correct_result() {
+        assert_eq!(problem(12), 3);
+        assert_eq!(problem(42), 7);
+        assert_eq!(problem(128), 2);
+        assert_eq!(problem(60100), 601);
+        assert_eq!(problem(600_851_475_143), 6857);
+    }
 
-        assert_eq!(_is_prime(4), false);
-        assert_eq!(_is_prime(6), false);
-        assert_eq!(_is_prime(16), false);
-        assert_eq!(_is_prime(21), false);
-        assert_eq!(_is_prime(30), false);
-        assert_eq!(_is_prime(98), false);
+    #[test]
+    fn get_prime_factorization_works() {
+        assert_eq!(get_prime_factorization(9), [3, 3]);
+        assert_eq!(get_prime_factorization(128), [2, 2, 2, 2, 2, 2, 2]);
+        assert_eq!(get_prime_factorization(42), [2, 3, 7]);
+        assert_eq!(get_prime_factorization(60100), [2, 2, 5, 5, 601]);
+        assert_eq!(get_prime_factorization(600_851_475_143), [71, 839, 1471, 6857]);
+    }
+
+    #[test]
+    fn is_prime_works() {
+        let mut primes = Primes::new();
+
+        assert_eq!(primes.is_prime(2), true);
+        assert_eq!(primes.is_prime(3), true);
+        assert_eq!(primes.is_prime(29), true);
+        assert_eq!(primes.is_prime(97), true);
+
+        assert_eq!(primes.is_prime(4), false);
+        assert_eq!(primes.is_prime(21), false);
+        assert_eq!(primes.is_prime(42), false);
     }
 }
